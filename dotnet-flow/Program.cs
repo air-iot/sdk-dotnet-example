642341cfd261cb86a7b5a310e230e16b35ad3525
张成 <1051764279@qq.com>
2023-09-08 17:38:01 +0800

    dotnet流程插件例子

---
+++ dotnet-flow/Program.cs
@@ -0,0 +1,27 @@
+﻿using AiriotSDK.Flow;
+using AiriotSDK.Tools;
+using System;
+using System.Threading;
+
+namespace netcore_flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FlowApp app = new();
+                app.Start(new TestFlow());
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ dotnet-flow/TestFlow.cs
@@ -0,0 +1,22 @@
+﻿using AiriotSDK.Data;
+using AiriotSDK.Flow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netcore_flow
+{
+    class TestFlow : IFlow
+    {
+        public CommResult Handler(IFlowApp app, Request req)
+        {
+            return CommResult.Success(null, new Dictionary<string, object>()
+                {
+                    { "a", 1 },
+                }
+            );
+        }
+    }
+}
